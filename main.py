f48a8a394f9646751936d202f207e2a230cd45b0
Carter Dennis <carter.j.dennis-1@ou.edu>
2021-07-28 18:38:16 -0500

    Rewrite !purge command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 
-import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -29,14 +28,17 @@ async def getCommands(ctx):
 
 # Purge messges from current channel
 @bot.command(name = 'purge', help = """Purges messages from current channel. Defaults to 
-                                       25 messages, use !purge <num> to delete set amount""")
-async def purge(ctx, amount = 25):
-    channel = ctx.message.channel
-    messages = []
-    async for message in channel.history(limit = amount + 1):
-        messages.append(message)
-
-    await channel.delete_messages(messages)
-    await ctx.send(f'{amount} messages purged by {ctx.message.author.mention}')
+                                       25 messages, use !purge <num> to delete set amount
+                                       or !purge all to clear entire channel.""")
+async def purge(ctx, amount = None):
+    if amount is None:
+        await ctx.channel.purge(limit = 25)
+        await ctx.send(f'25 messages purged by {ctx.message.author.mention}')
+    elif amount == "all":
+        await ctx.channel.purge()
+        await ctx.send(f'All messages purged by {ctx.message.author.mention}')
+    else:
+        await ctx.channel.purge(limit = int(amount))
+        await ctx.send(f'{amount} messages purged by {ctx.message.author.mention}')
 
 bot.run(TOKEN)
