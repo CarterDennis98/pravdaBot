6e65712d00693dfb3a09d9663666d2552472fb08
Carter Dennis <carter.j.dennis@gmail.com>
2021-08-08 20:46:13 -0500

    added !hc for each exaltation dungeon

--- main.py
+++ main.py
@@ -80,6 +80,7 @@ async def purge(ctx, amount=''):
                                   If you want to have spaces in your title/options make sure to put them 
                                   within double ("abc") quotes.""")
 async def poll(ctx, title='Poll', *options: str):
+    await ctx.channel.purge(limit=1)
     if len(options) <= 1 and len(options) != 0:
         # If there is 1 or negative options, tell user input is invalid
         await ctx.send(f'{ctx.message.author.mention}, you must enter more than one option to create a poll!',
@@ -99,7 +100,6 @@ async def poll(ctx, title='Poll', *options: str):
         await msg.add_reaction('✅')
         await msg.add_reaction('❌')
     else:
-        await ctx.send(f'Options: {options}')
         poll = discord.Embed(title=f'{title}', color=discord.Color.red(
         ), timestamp=datetime.datetime.utcnow())
         reactions = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣']
@@ -119,11 +119,12 @@ async def poll(ctx, title='Poll', *options: str):
                                 generate headcount for exaltation dungeons (!hc [exaltation]),
                                 generate rune check (!hc [runes], or generate headcount for all dungeons (!hc)""")
 async def hc(ctx, dungeon='all'):
+    await ctx.channel.purge(limit=1)
     if(dungeon == 'exaltations'):
         embed = discord.Embed(title='Headcount for Exaltation Dungeons', color=discord.Color.blue(),
                               timestamp=datetime.datetime.utcnow())
         embed.add_field(name=f'Headcount started for Exaltation Dungeons by {ctx.message.author.name}!',
-                        value="""React with any keys you are willing to pop or react with the corresponding portal if you wish to participate.""")
+                        value='React with any keys you are willing to pop or react with the corresponding portal if you wish to participate.')
         img = discord.File('images/exaltedWizard.png', filename='image.png')
         embed.set_thumbnail(url='attachment://image.png')
         msg = await ctx.send(file=img, embed=embed)
@@ -139,6 +140,8 @@ async def hc(ctx, dungeon='all'):
     elif(dungeon == 'o3' or dungeon == 'O3' or dungeon == 'Oryx 3'):
         embed = discord.Embed(title='Headcount for Oryx 3', color=discord.Color.blue(),
                               timestamp=datetime.datetime.utcnow())
+        embed.add_field(name=f'Headcount for Oryx 3 started by {ctx.message.author.name}!',
+                        value='React with <:osancPortal:874068648115982438> to participate or with <:shieldRune:874070197949071402> <:helmetRune:874070181675163658> <:swordRune:874070189141004429> <:inc:874079430002221077> if you have runes/inc you want to pop.')
         img = discord.File('images/o3.png', filename='image.png')
         embed.set_thumbnail(url='attachment://image.png')
         msg = await ctx.send(file=img, embed=embed)
@@ -150,11 +153,53 @@ async def hc(ctx, dungeon='all'):
     elif(dungeon == 'shatters' or dungeon == 'Shatters'):
         embed = discord.Embed(title='Headcount for The Shatters', color=discord.Color.blue(),
                               timestamp=datetime.datetime.utcnow())
+        embed.add_field(name=f'Headcount for The Shatters started by {ctx.message.author.name}!',
+                        value='React with <:shattersPortal:874068565559488522> to join or with <:shattersKey:874070162574311504> if you have keys you want to pop.')
         img = discord.File('images/forgottenKing.png', filename='image.png')
         embed.set_thumbnail(url='attachment://image.png')
         msg = await ctx.send(file=img, embed=embed)
         await msg.add_reaction('<:shattersPortal:874068565559488522>')
         await msg.add_reaction('<:shattersKey:874070162574311504>')
+    elif(dungeon == 'nest' or dungeon == 'Nest'):
+        embed = discord.Embed(title='Headcount for The Nest', color=discord.Color.blue(),
+                              timestamp=datetime.datetime.utcnow())
+        embed.add_field(name=f'Headcount for The Nest started by {ctx.message.author.name}!',
+                        value='React with <:nestPortal:874068640708853810> to join or with <:nestKey:874070151685898292> if you have keys you want to pop.')
+        img = discord.File('images/queenBee.png', filename='image.png')
+        embed.set_thumbnail(url='attachment://image.png')
+        msg = await ctx.send(file=img, embed=embed)
+        await msg.add_reaction('<:nestPortal:874068640708853810>')
+        await msg.add_reaction('<:nestKey:874070151685898292>')
+    elif(dungeon == 'fungal' or dungeon == 'Fungal'):
+        embed = discord.Embed(title='Headcount for Fungal Cavern', color=discord.Color.blue(),
+                              timestamp=datetime.datetime.utcnow())
+        embed.add_field(name=f'Headcount for Fungal Cavern started by {ctx.message.author.name}!',
+                        value='React with <:fungalPortal:874068627098337320> to join or with <:fungalKey:874070120132124684> if you have keys you want to pop.')
+        img = discord.File('images/crystalMother.png', filename='image.png')
+        embed.set_thumbnail(url='attachment://image.png')
+        msg = await ctx.send(file=img, embed=embed)
+        await msg.add_reaction('<:fungalPortal:874068627098337320>')
+        await msg.add_reaction('<:fungalKey:874070120132124684>')
+    elif(dungeon == 'cult' or dungeon == 'Cult'):
+        embed = discord.Embed(title='Headcount for Cultist Hideout', color=discord.Color.blue(),
+                              timestamp=datetime.datetime.utcnow())
+        embed.add_field(name=f'Headcount for Cultist Hideout started by {ctx.message.author.name}!',
+                        value='React with <:cultPortal:874068612032368640> to join or with <:hallsKey:874070141170749470> if you have keys you want to pop.')
+        img = discord.File('images/malus.png', filename='image.png')
+        embed.set_thumbnail(url='attachment://image.png')
+        msg = await ctx.send(file=img, embed=embed)
+        await msg.add_reaction('<:cultPortal:874068612032368640>')
+        await msg.add_reaction('<:hallsKey:874070141170749470>')
+    elif(dungeon == 'void' or dungeon == 'Void'):
+        embed = discord.Embed(title='Headcount for The Void', color=discord.Color.blue(),
+                              timestamp=datetime.datetime.utcnow())
+        embed.add_field(name=f'Headcount for The Void started by {ctx.message.author.name}!',
+                        value='React with <:voidPortal:874068593422262373> to join or with <:hallsKey:874070141170749470> if you have keys you want to pop.')
+        img = discord.File('images/void.png', filename='image.png')
+        embed.set_thumbnail(url='attachment://image.png')
+        msg = await ctx.send(file=img, embed=embed)
+        await msg.add_reaction('<:voidPortal:874068593422262373>')
+        await msg.add_reaction('<:hallsKey:874070141170749470>')
 
 
 bot.run(TOKEN)
